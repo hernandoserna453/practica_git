423a4a761661878196fa092686ae28c7f5a51d52
Hernando Serna <hernandoserna453>
2025-11-13 09:40:24 -0500

    feat: Agregamos funciones de calculos como el area cuadrada, el area triangular y suma, se realizo la creacion de un menu con diferentes opciones para realizar las matematicas agregadas

--- calculos.py
+++ calculos.py
@@ -1,4 +1,4 @@
 def areatri(base,altura): #Se encarga de devolver el area de un triangulo RECTANGULO, donde base es la BASE del triangulo y ALTURA es la altura del triangulo
-    return base*altura
+    return base*altura / 2
 def areacuadra(lado): #Se encarga de devolver el area de un cuadrado donde lado es el LADO de un cuadrado
     return lado*lado
--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-from funtions import sumar
-resutaldo = sumar(1,2)
-print(resutaldo)
-print("Hola Mundo")
+from calculos import areatri,areacuadra #Aqui se importar funciones de calculos
+from funtions import sumar #Aqui se importa funciones desde funtions
+print("Hola Mundo") #Imprime Hola Mundo
+
+menu_interactivo = True
+while menu_interactivo:
+    print("                                    ")
+    print("----------Menu principal-------------")
+    print("1 - Area triangular")
+    print("2 - Area cuadrada")
+    print("3 - Suma")
+    print("4 - Salir")
+    print("                     ")
+    opcion = input("Elije una opcion: ")
+
+    if opcion =="1":
+        base = float(input("Ingrese numero 1: "))
+        altura = float(input("Ingrese numero 2: "))
+        resultado = areatri(base,altura) 
+        print(f"Resultado Area Triangular: {resultado}")
+           
+
+    if opcion =="2":
+        lado = float(input("ingrese el valor del lado: "))
+        resultado = areacuadra(lado) 
+        print(f"El area cuadrada es: {resultado}")
+
+    if opcion =="3":
+        num1 = float(input("Ingrese numero 1: "))
+        num2 = float(input("Ingrese numero 2: "))
+        resultado = sumar(num1,num2)
+        print(f"Resultado de la suma: {resultado}")
+
+    if opcion =="4":
+        print("Saliendo del programa") 
+        break
+
